621935b727d879549b647424eacb828fb8ed2917
Wade <2513473008@qq.com>
2023-03-16 11:35:01 +0800

    Initial commit

---
+++ src/main/java/temp/demo01.java
@@ -0,0 +1,21 @@
+package temp;
+// 求最大公约数
+public class demo01 {
+    public static void main(String[] args) {
+        //法一
+       /* int a = 50;
+        int b = 37;
+        for (int i = a; i >= 1 ; i--) {
+            if (a % i == 0 && b % i ==0){
+                System.out.println("最大公约数: "+i);
+                break;
+            }
+
+
+
+        }*/
+
+
+        //法二: 辗转相除法
+    }
+}
